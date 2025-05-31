ab208b54ebb5233f80e0236bd6cbbe7094e2e35c
Zubaer3002 <your-email@example.com>
2025-05-31 19:28:34 +0600

    updated

--- Sheet/sheet#3/H_Sorting.c
+++ Sheet/sheet#3/H_Sorting.c
@@ -32,3 +32,39 @@ int main() {
    
     return 0;
 }
+
+/*
+
+H. Sorting
+time limit per test1 second
+memory limit per test64 megabytes
+Given a number N and an array A of N numbers. Print the numbers after sorting them.
+
+Note:
+
+Don't use built-in-functions.
+try to solve it with bubble sort algorithm or Selection Sort.
+for more information watch : https://www.youtube.com/watch?v=EnodMqJuQEo.
+Input
+First line contains a number N (0 < N < 103) number of elements.
+
+Second line contains N numbers ( - 100 ≤ Ai ≤ 100).
+
+Output
+Print the numbers after sorting them.
+
+Examples
+InputCopy
+3
+3 1 2
+OutputCopy
+1 2 3 
+InputCopy
+4
+5 2 7 3
+OutputCopy
+2 3 5 7 
+
+
+
+*/
