68d2ab771503777be0737f8a5143ce3e4660a6ae
Zubaer3002 <your-email@example.com>
2025-06-24 18:22:58 +0600

    updated

--- IntroductionToDataStructure/4_Module3/1_RangeSumQuery.cpp
+++ IntroductionToDataStructure/4_Module3/1_RangeSumQuery.cpp
@@ -2,57 +2,31 @@
 using namespace std;
 
 int main(){
+  
+  int n,q;
+  cin >> n >> q;
+  
+  vector<long long>v(n+1),pre(n+1);
+  
+  for(int i =1; i<=n; i++){
+    cin >> v[i];
+    pre[i] = pre[i-1] + v[i];
+  }
+  
+  
+  
+  while(q--){
     
-     int n,q;
-
-    //  int ar[n];
-
-    vector<long long int> v(n+1);
-
-     cin >> n>> q;
-
-     for(int i=1; i<=n;i++){
-        cin >> v[i];
-     }
-
-     vector<long long int> pre(n+1);
-     pre[1] = v[1];
-
-     for(int i=2; i<=n; i++){
-        pre[i] = pre[i-1] + v[i];
-     }
-
-
-
-     while (q--)
-     {
-        int l,r;
-        cin >> l >> r;
-        
-        long long int sum = 0;
-
-       if(l==1){
-
-        sum = pre[r];
-
-       }else{
-        sum = pre[r] - pre[l-1];
-       }
-        
-        cout << sum << endl;
-
-
-     }
-     
-
-
-
-
+    int l,r;
     
-
-    return 0;
+    cin >> l >> r;
+    
+    cout << pre[r] - pre[l-1] << endl;
+    
+  }
+  
+  
 }
-
 
 
 /*
