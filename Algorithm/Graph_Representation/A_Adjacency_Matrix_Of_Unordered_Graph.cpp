ffb95cf5c7f98387b00653b5d0c3466c25b7de12
Zubaer3002 <your-email@example.com>
2025-08-29 07:28:05 +0600

    up

---
+++ Algorithm/Graph_Representation/A_Adjacency_Matrix_Of_Unordered_Graph.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+   int n,e;
+   cin >> n >> e;
+   
+   int adj_mat[n][n];
+
+   for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+        adj_mat[i][j] = 0;
+    }
+   }
+
+   for(int i=0;i<n; i++){
+    for(int j=0; j<n;j++){
+        if(i==j){
+            adj_mat[i][j] = 1;
+        }
+    }
+   }
+
+   while(e--){
+    int a,b; 
+    cin >> a >> b;
+
+    adj_mat[a][b]= 1;
+    adj_mat[b][a] = 1;
+
+   }
+
+   for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+        cout << adj_mat[i][j] << " ";
+    }
+    cout << endl;
+   }
+    
+
+    return 0;
+}
+
+
+/*
+
+Input: 
+6 6
+0 1
+0 2
+0 4
+0 3
+1 4
+3 4
+
+Output: 
+1 1 1 1 1 0 
+1 1 0 0 1 0 
+1 0 1 0 0 0 
+1 0 0 1 1 0 
+1 1 0 1 1 0 
+0 0 0 0 0 1 
+
+
+Rules:
+1.set all 0 at initial stage in adj_mat
+2.diagonally set 1
+3.set value 1 from input as the condition of undirected graph.
+4.show output.
+
+
+*/
